8e7010f9f6b2dbf85afe6e8e423a0710ed3c6534
Sean Tilson <seantilson@gmail.com>
2024-04-07 19:31:42 +0000

    feat: adds body to displayAddition function
    feat: adds to displayAddition call to runGame body as well as unknown game type alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 let gameType = buttonType;
                 alert(`You clicked ${gameType}!`);
-                S
+                runGame(gameType);
             }
         })
     }
+    runGame('addition');
 })
 
 /**
  * The main game loop, called when script is first loaded
  * and when users answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
     // creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}.`);
+        throw `Unknown game type: ${gameType}. Aborting.`;
+    }
 }
 
 function checkAnswer() {
@@ -44,7 +52,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 
 }
 
