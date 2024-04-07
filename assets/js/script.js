adf24dfa60aa3d83b9887a23333a995d30ddaf5d
Sean Tilson <seantilson@gmail.com>
2024-04-07 19:07:45 +0000

    feat: adds event listener for DOM content load
    feat: adds event listeners for button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// Wait for the page to load before running game
+// Get button elements and add listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            let buttonType = this.getAttribute('data-type');
+            if (buttonType == 'submit') {
+                alert('You clicked Submit!');
+            } else {
+                let gameType = buttonType;
+                alert(`You clicked ${gameType}!`);
+                S
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -23,13 +43,13 @@ function displayAdditionQuestion() {
 }
 
 function displaySubtractQuestion() {
-    
+
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
 
 function displayDivisionQuestion() {
-    
+
 }
